ff631c9eea664a796281af4282a777db5b2b67b5
ismailjosim <ismailjosim99@gmail.com>
2023-02-10 17:28:08 +0600

    add redux thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
-import rootReducer from "./reducers/rootReducer";
-import cartCounter from "./middlewares/cartCounter";
-import thunk from "redux-thunk";
+import productReducer from './reducers/productReducer';
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(cartCounter, thunk))
-);
+const store = createStore(productReducer, composeWithDevTools());
 
 export default store;
