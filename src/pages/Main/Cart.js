93e139dee72f17a35251571f81f8da64f966f1fa
ismailjosim <ismailjosim99@gmail.com>
2023-02-15 23:35:59 +0600

    Add middleware

--- src/pages/Main/Cart.js
+++ src/pages/Main/Cart.js
@@ -6,11 +6,11 @@ const Cart = () => {
   const cart = useSelector((state) => state.product.cart);
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {cart
+      { cart
         .sort((a, b) => a.cartPosition - b.cartPosition)
         .map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
+          <ProductCard key={ product.model } product={ product } />
+        )) }
     </div>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from 'redux-logger';
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer, composeWithDevTools());
+
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
